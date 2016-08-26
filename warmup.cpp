c522e21129881eb14ed55e9082af3927aad4c1e0
Trond Wiggo Johansen <wiggoen@gmail.com>
2016-08-26 15:19:14 +0200

    Initialisation
    Trying to write out x value

---
+++ warmup.cpp
@@ -0,0 +1,12 @@
+/* Program to calculate the first derivative of arctan(x) */
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    float x = sqrt(2);
+    cout << x;
+    return 0;
+}
