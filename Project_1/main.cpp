5856ac0c9986332133eb686b0e5f5aa02bcea481
Trond Wiggo Johansen <wiggoen@gmail.com>
2016-09-15 20:19:33 +0200

    not quite there
    Program almost running. No errors, but there is something wrong.

--- Project_1/main.cpp
+++ Project_1/main.cpp
@@ -21,31 +21,30 @@ void gen_solve(int N, double* v) {
     double *x = new double[N+2];
     double h = 1.0/(N+1);
 
-    // initialize elements
-    for (int i = 0; i < N; i++) {
+    // Initialize elements
+    for (int i = 0; i < N+2; i++) {
         a[i] = -1.0;
         b[i] = 2.0;
-        b_tilde[i] = b[0];
         c[i] = -1.0;
-        f[i] = pow(h, 2)*100*exp(-10*(i+1)*h); // x[i] = ih
-        f_tilde[i] = f[0];
+        x[i] = i*h;
+        f[i] = pow(h, 2)*100*exp(-10*x[i]);
         v[i] = 0.0;
-        x[i] = 0.0;
     }
 
-    // Boundary conditions is done in the Python script v[0] = v[N+1] = 0
+    // Initial conditions
+    f_tilde[1] = f[1];
+    b_tilde[1] = b[1];
 
-    // decomposition and forward substitution
-    for (int i = 1; i < N; i++) {
-        b_tilde[i] = b[i] - a[i]*c[i-1]/b_tilde[i-1]; // new diagonal elements
-        //x[i] = i*h;
-        f_tilde[i] = f[i] - (a[i]*f_tilde[i-1])/b_tilde[i-1];
+    // Decomposition and forward substitution
+    for (int i = 2; i < N+1; i++) {
+        b_tilde[i] = b[i] - a[i-1]*c[i-1]/b_tilde[i-1]; // new diagonal elements
+        f_tilde[i] = f[i] - (a[i-1]*f_tilde[i-1])/b_tilde[i-1];
     }
-    v[N-1] = f_tilde[N-1]/b_tilde[N-1];
+    v[N] = f_tilde[N]/b_tilde[N];
 
-    // backward substitution
-    for (int i = N-1; i >= 0; i--) {
-        v[i-1] = (f_tilde[i-1]-c[i-1]*v[i])/b_tilde[i-1];
+    // Backward substitution
+    for (int i = N-1; i >= 1; i--) {
+        v[i] = (f_tilde[i]-c[i]*v[i+1])/b_tilde[i];
     }
 }   // End gen_solve
 
@@ -62,33 +61,26 @@ void special_solve(int N, double* v) {
     // initialize elements
     for (int i = 0; i < N+2; i++) {
         b[i] = 2.0;
-        //b_tilde[i] = b[0];
         x[i] = i*h;
         f[i] = pow(h, 2)*100*exp(-10*x[i]);
-        //f_tilde[i] = 0.0;
         v[i] = 0.0;
-        z[i] = 0.0;
     }
     // Initial conditions
     f_tilde[1] = f[1];
     b_tilde[1] = b[1];
 
-    // Boundary conditions is done in the Python script v[0] = v[N+1] = 0
-
     // decomposition and forward substitution
-    for (int i = 1; i < N+1; i++) {
+    for (int i = 2; i < N+1; i++) {
         z[i] = (i-1.0)/i;
         b_tilde[i] = (i+1.0)/i; // new diagonal elements, 1.0 to avoid integer division
-        //x[i] = i*h;
         f_tilde[i] = f[i] - z[i]*f[i-1];
     }
     v[N] = f_tilde[N]/b_tilde[N];
 
     // backward substitution
-    for (int i = N+1; i >= 1; i--) {
+    for (int i = N-1; i >= 1; i--) {
         v[i-1] = z[i]*(f_tilde[i-1]-v[i]);
     }
-    cout << sizeof(v) << endl;
 }   // End special_solve
 
 int main(int argc, char* argv[])
@@ -100,17 +92,17 @@ int main(int argc, char* argv[])
         exit(1);
     }
 
-    // declaring variables
+    // Declaring variables
     string outfilename = string(argv[1]) + "-" + string(argv[2]) + ".txt"; // first command line argument
     int N = atoi(argv[2]); // second command line argument
-    double* v = new double[N+2];
+    double* v = new double[N];
     clock_t start, finish;
 
     // Start clock
     start = clock();
 
     // Call solver
-    special_solve(N,v);
+    gen_solve(N,v);
 
     finish = clock();
     // End clock
@@ -122,13 +114,10 @@ int main(int argc, char* argv[])
 
     // open file and write results to file
     ofile.open(outfilename);
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < N+2; i++) {
         ofile << setw(15) << setprecision(8) << v[i] << endl;
     }
     ofile.close();
 
-    //delete[] v;
-
-    //cout << "Hello World!" << endl;
     return 0;
 }
