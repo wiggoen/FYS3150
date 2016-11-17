730a0133092188f22af392565f37b2c6e4944cd0
Trond Wiggo Johansen <wiggoen@gmail.com>
2016-11-17 12:51:11 +0100

    A start

--- Project_5/main.cpp
+++ Project_5/main.cpp
@@ -9,7 +9,7 @@ ofstream ofile;
 // Declaration of functions
 double RN();
 void MonteCarloSimulation(int N, int mcc);
-void exchange();
+void moneyExchange();
 
 
 int main()//int argc, char *argv[])
@@ -17,9 +17,12 @@ int main()//int argc, char *argv[])
     // Initial conditions
     int N = 500;             // Number of agents
     double m0 = 1000;        // Amount of money at start
-    double delta_m = 0.05;   // Typically 0.01 - 0.05
+    double delta_m = 0.05;   // Typically [0.01, 0.05]
     int transactions = 1e7;  // Number of transactions
-    int mcc = 5;             // Monte Carlo cycles (1e3 - 1e4 runs)
+    int mcc = 5;             // Monte Carlo cycles ([1e3, 1e4] runs)
+
+    double lambda = 0.25;    // {0.25, 0.5, 0.9}
+    // (int or double?) alpha  // [1, 2]
 
     MonteCarloSimulation(N, mcc);
 
@@ -36,19 +39,21 @@ double RN() {
 
 void MonteCarloSimulation(int N, int mcc) {
     for (int cycles = 1; cycles <= mcc; cycles++) {
-        double r = RN();    // Random number
+        double epsilon = RN();    // Random number
         int rint = RN()*5;  // Random integer between 0 - 5
-        cout << "Random number = " << r << " and random int = " << rint << endl;
+        cout << "Random number = " << epsilon << " and random int = " << rint << endl;
 
-        // Find random transaction pair
+        // Find a random pair of agents
         int i = RN()*N;
         int j = RN()*N;
+        moneyExchange();
 
         cout << "i = " << i << "  and  j = " << j << endl;
 
     }
 }
 
-void exchange() {
-
+void moneyExchange() {
+    double mi_new = epsilon*(mi + mj);
+    double mj_new = (1 - epsilon)*(mi + mj);
 }
