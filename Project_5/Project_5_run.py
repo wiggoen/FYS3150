f30f9d3d58c5765d55f24082e3b1ceffec2f1fc4
Trond Wiggo Johansen <wiggoen@gmail.com>
2016-12-01 14:55:56 +0100

    added gamma

--- Project_5/Project_5_run.py
+++ Project_5/Project_5_run.py
@@ -4,29 +4,30 @@ import time
 # System setup
 N = [500, 1000]
 m0 = 1.0
-transactions = 1e7
-runs = 1e4
+transactions = 10
+runs = 10
 Alpha = [0.5, 1.0, 1.5, 2.0]
-A = ["a0d5", "a1", "a1d5", "a2"]
+A = ["a05", "a1", "a15", "a2"]
 Lambda = [0.25, 0.5, 0.9]
-L = ["L0d25", "L0d5", "L0d9"]
+L = ["L025", "L05", "L09"]
 Gamma = [0.0, 1.0, 2.0, 3.0, 4.0]
 G = ["g0", "g1", "g2", "g3", "g4"]
 
 # Compile C++ code first (all cpp-files)
 system("g++ *.cpp -o main.x -O3 -std=c++11")
 
-for k in range(len(N)):
-    for j in range(len(Alpha)):
-        for i in range(len(Lambda)):
-            print "---"
-            print "Starting to run the program for N = %g, L = %1.2f and A = %1.2f." %(N[k], Lambda[i], Alpha[j])
-            billOfAbundance = "noEqualityForFatCats_N"+str(N[k])+"_"+L[i]+"_"+A[j]+".txt"
-            # Command line argument
-            systemStr = "./main.x %s %d %f %d %d %f %f" % (billOfAbundance, N[k], m0, transactions, runs, Alpha[j], Lambda[i])
-            start = time.time()         # Start clock
-            system(systemStr)           # Run program
-            finish = time.time()        # End clock
-            runtime = finish - start 
-            print "The python runtime of the program for L = %g was %f s." %(Lambda[i], runtime)
+for m in range(len(Gamma)):
+    for k in range(len(N)):
+        for j in range(len(Alpha)):
+            for i in range(len(Lambda)):
+                print "---"
+                print "Starting to run the program for N = %g, L = %g, A = %g and G = %g." %(N[k], Lambda[i], Alpha[j], Gamma[m])
+                billOfAbundance = "noEqualityForFatCats_N"+str(N[k])+"_"+L[i]+"_"+A[j]+"_"+G[m]+".txt"
+                # Command line argument
+                systemStr = "./main.x %s %d %f %d %d %f %f %f" % (billOfAbundance, N[k], m0, transactions, runs, Alpha[j], Lambda[i], Gamma[m])
+                start = time.time()         # Start clock
+                system(systemStr)           # Run program
+                finish = time.time()        # End clock
+                runtime = finish - start 
+                print "The python runtime of the program for N = %g, L = %g, A = %g and G = %g was %f s." %(N[k], Lambda[i], Alpha[j], Gamma[m], runtime)
 
